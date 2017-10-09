0487df73077f5d9834bb9702d8842bde1a83cdbf
mayur <mayurkhatri122@gmail.com>
2017-10-09 19:12:11 +0530

    Initial working version of url shortner - demo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def index
+    test = Shortener::ShortenedUrl.generate(params[:user_input])
+    sql = "SELECT * FROM shortened_urls order by use_count DESC"
+    @results = ActiveRecord::Base.connection.execute(sql)
   end
 end
---
+++ config/initializers/shortner.rb
@@ -0,0 +1,3 @@
+Shortener.unique_key_length = 6
+Shortener.default_redirect = "http://www.someurl.com"
+Shortener.charset = :alphanumcase
